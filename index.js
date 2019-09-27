e864178db30fe103ba95b02220bef063337fc442
John Dinh <johnduydinh@gmail.com>
2019-09-27 17:47:11 -0400

    Fix music bug

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ import CharacterCreation from "./modules/CharacterCreation";
 import createListener from "./functions/createListener";
 
 const gameWindow = document.querySelector(".game");
+const startWindow = gameWindow.querySelector(".start");
+const startBtn = startWindow.querySelector("button");
 const createWindow = gameWindow.querySelector(".creation");
 const music = createWindow.querySelector("audio");
 const form = createWindow.querySelector(".creation__input-form");
 
 let MainCharacter = JSON.parse(localStorage.getItem("MainCharacter"));
 
-if (!MainCharacter) {
-    createWindow.style.display = "block";
-    form.addEventListener("submit", createListener);
+
+
+function startListener() {
     music.play();
+    startWindow.style.display = "none";
+    createWindow.style.display = "block";
+}
+
+startBtn.addEventListener("click", startListener);
+
+if (!MainCharacter) {
+
+    form.addEventListener("submit", createListener);
 } else {
     console.log("yeet");
 }
