09f66e7e2b7db74395eb0a1b969e35fd6848520b
John Dinh <johnduydinh@gmail.com>
2019-09-28 10:53:29 -0400

    add new resources for scene one

--- functions/createListener.js
+++ functions/createListener.js
@@ -1,11 +1,19 @@
 import CharacterCreation from "../modules/CharacterCreation";
-
+const clickSound = document.querySelector(".click-sound")
+const sceneOne = document.querySelector(".scene-one");
+const sceneOneMusic = sceneOne.querySelector("audio");
 function createListener(e) {
+    clickSound.play();
+    sceneOneMusic.play();
     e.preventDefault();
     const name = this.querySelector("input").value;
     this.querySelector("input").value = "";
     localStorage.setItem("MainCharacter", JSON.stringify(new CharacterCreation({ name })));
-    this.parentElement.style.display = "none";
+    this.parentElement.parentElement.querySelector(".scene-one").style.display = "block";
+    this.parentElement.parentElement.querySelector(".scene-one").style.visibility = "visible";
+    this.parentElement.parentElement.querySelector(".scene-one").classList.add("fade-in");
+    document.querySelector(".scene-one__flame").classList.add("fade-in");
+    this.parentElement.remove();
 
 }
 
--- index.js
+++ index.js
@@ -9,14 +9,24 @@ const startBtn = startWindow.querySelector("button");
 const createWindow = gameWindow.querySelector(".creation");
 const music = createWindow.querySelector("audio");
 const form = createWindow.querySelector(".creation__input-form");
+const sceneOne = gameWindow.querySelector(".scene-one");
+const clickSound = document.querySelector(".click-sound");
+const sceneOneMusic = sceneOne.querySelector("audio");
 
 let MainCharacter = JSON.parse(localStorage.getItem("MainCharacter"));
 startBtn.addEventListener("click", e => {
-    music.play();
-    startWindow.style.display = "none";
+    clickSound.play();
+    startWindow.remove();
     if (!MainCharacter) {
-        createWindow.style.display = "block";
+        music.play();
+        createWindow.style.visibility = "visible";
+        createWindow.classList.add("fade-in")
         form.addEventListener("submit", createListener);
+    } else {
+        createWindow.style.display = "none";
+        sceneOne.style.visibility = "visible";
+        sceneOne.querySelector("img").classList.add("fade-in");
+        sceneOneMusic.play();
     }
 })
 
