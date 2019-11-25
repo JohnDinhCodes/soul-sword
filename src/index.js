2d96e3408582f97750c1c9f60baf87edf9074dc0
John Dinh <johnduydinh@gmail.com>
2019-11-25 14:51:32 -0500

    change canvas dimensions

--- src/index.js
+++ src/index.js
@@ -2,25 +2,25 @@ import Loading from "./scenes/Loading";
 import HomeTown from "./scenes/HomeTown";
 
 const config = {
-  width: 400,
-  height: 300,
-  zoom: 2,
-  type: Phaser.AUTO,
-  parent: "game",
-  scene: [Loading, HomeTown],
-  title: "Soul Sword",
-  version: "0.0.1 BETA",
-  autoFocus: true,
-  disableContextMenu: true,
-  render: {
-    pixelArt: true
-  },
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 0 }
+    width: 480,
+    height: 270,
+    zoom: 2,
+    type: Phaser.AUTO,
+    parent: "game",
+    scene: [Loading, HomeTown],
+    title: "Soul Sword",
+    version: "0.0.1 BETA",
+    autoFocus: true,
+    disableContextMenu: true,
+    render: {
+        pixelArt: true
+    },
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 0 }
+        }
     }
-  }
 };
 
 const game = new Phaser.Game(config);
