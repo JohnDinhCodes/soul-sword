3a65c1aff68bd69e3b8623f6ed7963aaf8ec9ef2
John Dinh <johnduydinh@gmail.com>
2019-11-26 21:07:41 -0500

    move event listeners for the up and down keys out of else statement

--- src/scenes/HomeTown.js
+++ src/scenes/HomeTown.js
@@ -295,16 +295,6 @@ class HomeTown extends Phaser.Scene {
 				this.player.speed = playerData.speed;
 			});
 		} else {
-			this.cursors.down.on('down', () => {
-				if (this.choiceIsOn) {
-					dialoguePlugin.toggleChoice();
-				}
-			});
-			this.cursors.up.on('down', () => {
-				if (this.choiceIsOn) {
-					dialoguePlugin.toggleChoice();
-				}
-			});
 			// Action Button
 			this.input.keyboard.on('keydown_X', () => {
 				this.actionButton();
@@ -321,6 +311,16 @@ class HomeTown extends Phaser.Scene {
 				this.player.isRunning = false;
 			});
 		}
+		this.cursors.down.on('down', () => {
+			if (this.choiceIsOn) {
+				dialoguePlugin.toggleChoice();
+			}
+		});
+		this.cursors.up.on('down', () => {
+			if (this.choiceIsOn) {
+				dialoguePlugin.toggleChoice();
+			}
+		});
 	}
 
 	actionButton() {
